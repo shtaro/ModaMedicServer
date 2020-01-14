628cfbd4f7125383b457e45ed55159ae0d8fd63b
shtaro <shtaro@post.bgu.ac.il>
2020-01-14 17:40:02 +0200

    changed port

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', indexRouter);
+//app.use('http://icc.ise.bgu.ac.il/njsw03:8103/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/questionnaires', questionnairesRouter);
 app.use('/metrics', metricsRouter);
