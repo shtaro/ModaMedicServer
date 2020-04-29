2ba43210752d75aaf7ceb6dce2cb90aa5c030de7
maore <maorelfassy@gmail.com>
2020-04-29 21:32:28 +0300

    add start on boot feature

---
+++ bin/startOnBoot.js
@@ -0,0 +1,21 @@
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+    name:'ModaMedicServer',
+    description: 'server',
+    script: 'C:\\Users\\maore\\WebstormProjects\\ModaMedicServerNEW\\bin\\www'
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+    svc.start();
+});
+
+svc.install();
+//svc.uninstall(); //change to this when want to REMOVE task and run locally
+
+svc.on('uninstall',function () {
+    svc.stop();
+});
